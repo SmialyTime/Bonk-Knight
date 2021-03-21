5b10a850e83c40b01cc29e2e0e0964d78b905f57
SmialyTime <Smialy_time1@iinet.net.au>
2021-03-21 13:08:50 +0800

    testing comit

--- Bonk_Knight/Program.cs
+++ Bonk_Knight/Program.cs
@@ -6,7 +6,7 @@ namespace Bonk_Knight
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello dude!");
+            Console.WriteLine("my dude!");
         }
     }
 }
