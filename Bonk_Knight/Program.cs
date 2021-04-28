c36be2a184cb6dbeaa78bf28c759b107082e0fdd
Oliver Powell <1012762@student.ccgs.wa.edu.au>
2021-04-28 17:46:30 +0800

    Set up a breif screen tested speed

--- Bonk_Knight/Program.cs
+++ Bonk_Knight/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Bonk_Knight
 {
@@ -6,18 +8,36 @@ namespace Bonk_Knight
     {
         public /*const*/ static int SSHeight = 15;
         public /*const*/ static int SSWidth = 15;
+        //public static List<List<Char>> Screen = new List<List<Char>>{}  ;
+        //Globals.Screen[rw,cl]
+        public static char[,] Screen = {{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+                                       ,{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+                                       ,{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+                                       ,{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+                                       ,{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+                                       ,{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+                                       ,{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+                                       ,{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+                                       ,{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }};
     }
+
     class MainClass
     {
          
         public static void Main(string[] args)
         {
             //Console.WriteLine("my dude thats cool!");
-            //AsciiArtTesting.funWAscii();
+            //AsciiArtTesting.funWAscii();  
+            String lettre = "-" ;
+            for (var i = 0; i < 1000; i++)
+            {
+                lettre = Convert.ToString("abcdefghijklmnopqrstuvwxyz"[(new Random()).Next(25)]);
+                ChangeScreen(1, 1, lettre);
+            }
             Console.WriteLine("having fun?");
             //Animations.MovingCloud(1);
 
-            TestingEvents EventRunnerGo = new TestingEvents();
+            //TestingEvents EventRunnerGo = new TestingEvents();
 
             //come back to - Animations.HoleInRect();
             //remove later
@@ -26,6 +46,35 @@ namespace Bonk_Knight
             Console.WriteLine("press any button to continue");
             Console.ReadLine();
             Console.ReadKey();
+        }
+
+        public static void ChangeScreen(int Row, int Column, String ToAdd)
+        {
+            //adds ToAdd it at the Row and Columb (from top left corner) if it will fit
+            for (int rw = 0;rw<9;rw++)
+            {
+                for (int cl = 0; cl < 30; cl++)
+                {
+                    Globals.Screen[rw, cl] = ToAdd[0];
+                }
+            }
+            RenderScreen();
+
+        }
+        public static void RenderScreen()
+        {
+            Console.SetCursorPosition(0, 0);
+            //render surrounding box when game initialised -------------------------------add in later
+            for (int rw = 0; rw < 9; rw++)
+            {
+                for (int cl = 0; cl < 30; cl++)
+                {
+                    Console.Write(Globals.Screen[rw,cl]);
+                }
+                Console.SetCursorPosition(0,Console.CursorTop+1);
+            }
+            //to ref the screen array use Screen[row][columb] & double nested for loops
+
         }
     }
 }
